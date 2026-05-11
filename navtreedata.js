805d0717cec7093ce1880b191e171c05d5141236
sasobadovinac <sasobadovinac@users.noreply.github.com>
2026-05-11 14:39:58 +0000

    deploy: 850deb323abca942418418c124fabfea8678333d

---
+++ navtreedata.js
@@ -0,0 +1,117 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Coin", "index.html", [
+    [ "New Features in Coin", "coin_new_features.html", "coin_new_features" ],
+    [ "Books Related to Open Inventor / Coin", "coin_books.html", [
+      [ "The Inventor Mentor", "coin_books.html#mentorbook", null ],
+      [ "The Inventor Toolmaker", "coin_books.html#toolmakerbook", null ],
+      [ "The Annotated VRML2.0 Reference Manual", "coin_books.html#vrml2refbook", null ],
+      [ "The Open GL Programming Manual", "coin_books.html#openglbook", null ],
+      [ "The Open Inventor C++ Reference Manual", "coin_books.html#oirefbook", null ]
+    ] ],
+    [ "Related Pages", "coin_related_pages.html", "coin_related_pages" ],
+    [ "Topics", "topics.html", "topics" ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", "functions_eval" ],
+        [ "Related Symbols", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"CoinResources_8h_source.html",
+"SoEngineOutputList_8h_source.html",
+"SoPickedPointList_8h_source.html",
+"SoVRMLColor_8h_source.html",
+"classSbBox3d.html#a4770f04a2ad3da733a3057a49cb73668",
+"classSbDPLine.html#a01f65400965ff8aa1627b09d4a1223a0",
+"classSbList.html#ad92621e814796448b9fe0990bdb959ab",
+"classSbSphereProjector.html#a729fa28d6220350e6352491dcfd74e73",
+"classSbVec2i32.html#a17b0be4cc751a41ff8423359ed754c03",
+"classSbVec4d.html#ad28f16fc9f57cab7b39566693e5a9246",
+"classScXMLEqualsOpExprDataObj.html#a212d86df462e6d05ce529435380e3e62",
+"classSoAnnoText3FontSizeHintElement.html#a98f0d14bf9b70be20542b622b125d15aa3d87032f51a21767617c9934d52ff347",
+"classSoBoolOperation.html#a2787555d844bdc6321f13e05963ee3c7",
+"classSoCallbackAction.html#a799242a77801b3170c49c04671886c8a",
+"classSoComposeRotationFromTo.html#a054cc01b1fc1f12967ec90f76f818d12",
+"classSoDecomposeMatrix.html#a181aa5ce78179323d129b697ae73d47b",
+"classSoDrawStyle.html#a3465e14668a02cd28f2f53dd7940b0c7",
+"classSoExtSelection.html#a312b1897793d9a7f78e4624d9a63e115",
+"classSoFont.html#ab69f7ddb085b9ca87d99176dc0f82ee8",
+"classSoGLMultiTextureImageElement.html#a26d849d55c14961aad89c8711256da48",
+"classSoGeometryShader.html#a1c2c5367c014c18e7fc3958935b947e0a4458b1d3f97a352519d17de2785724be",
+"classSoIndexedNurbsCurve.html",
+"classSoIntersectionDetectionAction.html#a789ee88aa501678c6183150e3db487f9",
+"classSoLightModelElement.html#ad0723bdf5580594fb4f5e01922479cdb",
+"classSoMFMatrix.html#accdb4f897af56021ffe4a89d63c44289",
+"classSoMFVec4f.html#a99c7eac85ab025457a5709d7322ceca8",
+"classSoMemoryError.html",
+"classSoNodeList.html#a5f8cdd4e4ce2f1751e860e2198bc6fac",
+"classSoNurbsSurface.html#aaec204eb1b27b630ae589a7fa902d2f9",
+"classSoPathSensor.html#a7ef4a67b977aba476b25c44d839099af",
+"classSoProfile.html#a3968857494b068a53e0bd6b790fd45eb",
+"classSoRenderManager.html#ac71a45dc1419420a3128b22790172e8b",
+"classSoSFEnum.html#a8ce8f4e1ff5c5b999b55accdd72b330e",
+"classSoScXMLFlightControlTarget.html#a41e426aa1e15ee506d1dfff2951c7138",
+"classSoSelectOne.html",
+"classSoShaderParameterArray4f.html#a757befa18bc4cde43ebce3d602155884",
+"classSoSpaceballButtonEvent.html#ac1a78099c48e9ca031d4abdbcb77a2bea59923e9db840c08b72a2a7aea8f791fe",
+"classSoTexture2.html#a262a667689ef20bb704803f78ca2a0b1ae4162c6a9bb8c157aa996eff119a9b8c",
+"classSoTextureCoordinateReflectionMap.html",
+"classSoTransformBoxDragger.html#ab23a5523cf09a0a71e87c7d77c4fa541",
+"classSoUnits.html#a3cbd1f8310f96411faf8f2f8ca1c2e16",
+"classSoVRMLDragSensor.html#aeb834bf534b9f012893cf909b4a0eeff",
+"classSoVRMLMovieTexture.html",
+"classSoVRMLSphereSensor.html#a40e81d1963f438f22b65139dbd17caec",
+"classSoVRMLVisibilitySensor.html#a21557cd67fe0c42afd497b7a6ca910ed",
+"classSoWWWInline.html#a5337971a1acc874c2dcc556a628ee256",
+"functions_vars_b.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ search/pages_8.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['8_0',['New Features in Coin v4.0.8',['../coin408.html',1,'coin_new_features']]]
+];
